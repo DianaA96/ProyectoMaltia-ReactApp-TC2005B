2274af37174b5918abea7dede2bd8f52a9fb97cd
Emmanuel Bolteada <a01276310@itesm.mx>
2021-03-07 19:00:39 -0600

    Quita texto a label de Checkbox

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
 ReactDOM.render(
   <React.StrictMode>
     <p>Prueba</p>
