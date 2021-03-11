088ecc5e7101baad6a38e5059687e9412032b084
Diana Aguirre <>
2021-03-11 03:09:44 -0600

    Cambios en el codigo para permitir el ruteo a partir de los elementos de la barra lateral

--- src/Lateral.js
+++ src/Lateral.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './Lateral.css';
-import TabLateral from './TabLateral'
+import TabLateral from './TabLateral';
+
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch,
+  } from 'react-router-dom';
 
 function Lateral(props){
     return(
@@ -12,17 +19,18 @@ function Lateral(props){
                     <h4 className="nombre_usuario">{props.usuario}</h4>
             </div>
 
+            
             <div className='tabs'>
                 {props.tabs.map((ventana) =>
-                    <TabLateral key={ventana} name={ventana} />
+                    <TabLateral key={ventana} name={ventana}/>
                 )}
             </div>
+                
 
             <div className="logout">
-                <a><i class="fas fa-sign-out-alt"></i></a>
-                <p>Cerrar sesión</p>
+                <a><Link to='./login'><i class="fas fa-sign-out-alt"></i></Link></a>
+                <p><Link to='./login'>Cerrar sesión</Link></p>
             </div>
-             
             
         </div>
     );
