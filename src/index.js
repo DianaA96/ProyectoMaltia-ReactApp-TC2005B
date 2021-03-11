f27c367d87582c779968a5258d841db59e52b1f1
HeronSamperio <joseheronsamperio@gmail.com>
2021-03-10 19:34:33 -0600

    arreglo ventana modal de conacto asesor y se le modifica el tamaño

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ModalContactosAsesor from './ContactoAsesor.js';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -7,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <p>Prueba</p>
+    <ModalContactosAsesor/>
   </React.StrictMode>,
   document.getElementById('root')
 );
