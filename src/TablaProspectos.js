ec238db2894a354076d11297ed3dac68c39b5374
Diana Aguirre <>
2021-03-12 16:25:02 -0600

    Importación de la librería react-router-dom para ruteo del componente.

--- src/TablaProspectos.js
+++ src/TablaProspectos.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import './TablaProspectos.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+} from 'react-router-dom';
+
 function TablaProspectos(props) {
     return (
-      <section className="contenedorTabla">
+      <section className="contenedorTablaProspectos">
          <div className="encabezadoTabla">
              <h5 id="encabezadoNombreProspecto">Nombre del prospecto</h5>
              <h5 id="encabezadoAccionesProspecto">Acciones</h5>
          </div>
           <section className="tabla">
-                  <table className="rwd-table">
-                        {props.data.map((registro, indice) => (
-                        <tr>
-                        <td data-th="nombreProspecto">{registro.nombres + " " + registro.apellidos}</td>
-                        <td data-th="botonEditar"><button id="botonEditarProspecto">Editar</button></td>
-                        <td data-th="botonContactar"><button id="botonContactarProspecto">Contactar</button></td>
-                        <td data-th="botonIniciarSolicitud"><button id="botonIniciarSolicitudProspecto">Iniciar solicitud</button></td>
-                        </tr>
-                        ))}
-                  </table>
-        </section>
+              <table className="rwd-table">
+                  {props.data.map((registro, indice) => (
+                  <tr>
+                  <td data-th="nombreProspecto">{registro.nombres + " " + registro.apellidos}</td>
+                  <td data-th="botonEditar"><button id="botonEditarProspecto"><Link to='./editarProspecto'>Editar</Link></button></td>
+                  <td data-th="botonContactar"><button id="botonContactarProspecto"><Link to='./contactarProspecto'>Contactar</Link></button></td>
+                  <td data-th="botonIniciarSolicitud"><button id="botonIniciarSolicitudProspecto"><Link to='./SolicitudCliente'>Iniciar solicitud</Link></button></td>
+                  </tr>
+                  ))}
+              </table>
+          </section>
      </section>
     );
   }
