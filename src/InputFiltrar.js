2e9a453ce746f0c1dfe246bd8ca03093b37e3a4d
Diana Aguirre <a01276380@itesm.mx>
2021-04-12 03:33:06 -0500

    Cambio de estructura del componente para adecuarlo a vista móvil

--- src/InputFiltrar.js
+++ src/InputFiltrar.js
@@ -3,7 +3,7 @@ import './InputFiltrar.css'
 function InputFiltrar(){
     return(
         <div className="filterInput">
-            <input id="searchFilter" type="text" placeholder="Escribe para filtrar"/>
+            <input className="input-search" type="search" placeholder="Escribe para filtrar"/>
             <button className="buttonInputSearch"><i class="fas fa-filter"></i></button>
             <ul className="dropFilter"></ul>
         </div>
