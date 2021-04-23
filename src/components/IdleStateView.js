5724fd01606cdda6ecb3326de1e3f4cc53c097f7
Diana Aguirre <a01276380@itesm.mx>
2021-04-23 04:22:56 -0500

    Adición del componente que se renderea en estado idle para señalar carga de los recursos

---
+++ src/components/IdleStateView.js
@@ -0,0 +1,11 @@
+import "./IdleStateView.css"
+
+function idleStateView() {
+    return(
+        <div className="center">
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default idleStateView;
