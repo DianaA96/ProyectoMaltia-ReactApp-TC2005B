e9ec760237604b9591b8f7f9b513c51cc0646739
Diana Aguirre <a01276380@itesm.mx>
2021-04-25 17:57:30 -0500

    Cambios menores

--- src/components/TablaUsuarios.js
+++ src/components/TablaUsuarios.js
@@ -6,65 +6,65 @@ import IdleStateView from './IdleStateView';
 import ErrorScreen from './ErrorScreen';
 
 function TablaUsuarios(props) {
-
-      function showModal(){
-            props.setStatus('visible')
-      }
-
-      const [status, setStatus ] = useState('idle');
-      const [error, setError] = useState(null);
-      const [employees, setEmployees] = useState([]);
-
-      useEffect(()=>{
-            setStatus('loading')
-            axios.get('http://localhost:5000/employees')
-                .then((result)=>{
-                    setEmployees(result.data.empleados)
-                    setStatus('resolved')
-                })
-                .catch((error)=>{
-                    setError(error)
-                    setStatus('error')
-                })
-        }, [])
-
-        if(status === 'idle' || status === 'loading'){
-            return <IdleStateView></IdleStateView>
-        }
-
-
-        if(status === 'error'){
-            return (
-                <ErrorScreen mensaje = {error.message} respuesta={error.name}/>
-            )
-        }
-
-      if(status === 'resolved'){
-
-            return (
-                  <section className="contenedorTablaUsuarios">
-                        <div className="encabezadoTablaUsuarios">
-                              <h5 id="encabezadoNombreUsuario">Nombre del usuario</h5>
-                              <h5 id="encabezadoRolUsuario">Rol</h5>
-                              <h5 id="encabezadoAccionesUsuario">Acciones</h5>
-                        </div>
-                        <section className="tablaUsuarios">
-                              <table className="userTable">
-                              {employees.map((registro, indice) => (
-                                    <tbody>
-                                          <tr key={indice}>
-                                                <td data-th={`\u000A ${registro.puesto}`}>{registro.nombre + " " + registro.apellidoPaterno + " " + registro.apellidoMaterno}</td>
-                                                <td>{registro.puesto}</td>
-                                                <td><CustomLink tag='button' to={`/editarUsuario/${registro.idEmployee}`} id="botonEditarUsuario"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></CustomLink></td>
-                                                <td><button onClick={showModal} tag='button' id="botonEliminarUsuario"><i class="fas fa-user-slash"></i></button></td>
-                                          </tr>
-                                    </tbody>
-                              ))}
-                              </table>
-                        </section>
-            </section>
-            );
-      }
+  
+  function showModal(){
+    props.setStatus('visible')
   }
+  
+  const [status, setStatus ] = useState('idle');
+  const [error, setError] = useState(null);
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(()=>{
+    setStatus('loading')
+    axios.get('http://localhost:5000/employees')
+      .then((result)=>{
+        setEmployees(result.data.empleados)
+        setStatus('resolved')
+      })
+      .catch((error)=>{
+        setError(error)
+        setStatus('error')
+      })
+}, [])
+
+if(status === 'idle' || status === 'loading'){
+    return <IdleStateView></IdleStateView>
+}
+
+
+if(status === 'error'){
+    return (
+        <ErrorScreen mensaje = {error.message} respuesta={error.name}/>
+    )
+}
+
+if(status === 'resolved'){
+
+  return (
+    <section className="contenedorTablaUsuarios">
+      <div className="encabezadoTablaUsuarios">
+        <h5 id="encabezadoNombreUsuario">Nombre del usuario</h5>
+        <h5 id="encabezadoRolUsuario">Rol</h5>
+        <h5 id="encabezadoAccionesUsuario">Acciones</h5>
+      </div>
+    <section className="tablaUsuarios">
+      <table className="userTable">
+      {employees.map((registro) => (
+        <tbody>
+          <tr key={registro.idEmployee}>
+            <td data-th={`\u000A ${registro.puesto}`}>{registro.nombre + " " + registro.apellidoPaterno + " " + registro.apellidoMaterno}</td>
+            <td>{registro.puesto}</td>
+            <td><CustomLink tag='button' to={`/editarUsuario/${registro.idEmployee}`} id="botonEditarUsuario"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></CustomLink></td>
+            <td><button onClick={showModal} tag='button' id="botonEliminarUsuario"><i class="fas fa-user-slash"></i></button></td>
+          </tr>
+        </tbody>
+      ))}
+      </table>
+    </section>
+  </section>
+  );
+  }
+}
   
 export default TablaUsuarios;
