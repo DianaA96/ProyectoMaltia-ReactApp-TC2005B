eee6861378866bb4a7c548ba67bb6432e069319a
HeronSamperio <joseheronsamperio@gmail.com>
2021-04-26 14:42:22 -0500

    Iniciar solicitud get y post D:

--- src/views/SolicitudCliente.js
+++ src/views/SolicitudCliente.js
@@ -5,16 +5,66 @@ import '../components/plantillaInputs.css';
 import Bienvenida from '../components/Bienvenida';
 import Lateral from '../components/Lateral';
 import Checkbox from '../components/Checkbox';
-import DropMenu from '../components/DropMenu';
 import asesor from '../assets/asesor.png';
 import CustomLink from '../components/CustomLink';
+import Select from 'react-select';
 import axios from 'axios';
 
 function SolicitudCliente(props) {
     const [status, setStatus]= useState('idle');
     const [datos , setDatos] = useState([]);
-    //const [info, setInfo] = useState({});
-    
+    const [error, setError] = useState(null);
+    const [SelectValue, setSelectValue] = useState([]);
+    const [fullApplication, setFullApplication] = useState({});
+    const [isCheck, setIsCheck] = useState(false);
+    const idAssessor= "1"
+
+    const customSelectStyles = {
+        control: (base, state) => ({
+            ...base,
+            background: "#CACACA",
+            borderRadius: "50px" ,
+            boxShadow: state.isFocused ? null : null,
+            padding: "7px 30px",
+            fontSize: "1.2vw",
+            fontFamily: "Raleway",
+            fontWeight: "600",
+            marginTop: "1em",
+            "@media only screen and (max-width: 576px)": {
+                ...base["@media only screen and (max-width: 576px)"],
+                background:"#F2F5FA",
+            },
+          }),
+          menu: base => ({
+            ...base,
+            borderRadius: "25px",
+            fontSize: "1.2vw",
+            fontFamily: "Raleway",
+            
+          }),
+          menuList: base => ({
+            ...base,
+            padding: 0,
+            borderRadius: "25px",
+          }),
+          dropdownIndicator: base => ({
+            ...base,
+            color: "#0f123f"
+          }),
+          container: base => ({
+            ...base,
+            width:"46%",
+            "@media only screen and (max-width: 576px)": {
+                ...base["@media only screen and (max-width: 576px)"],
+                width:"90%",
+            },
+          })
+    }
+
+    const tiposCredito = [
+        {value: 'simple' , label: 'Simple'},
+        {value: 'revolvente' , label: 'Revolvente'}
+    ]
 
     const{
         nombre,
@@ -23,7 +73,7 @@ function SolicitudCliente(props) {
         numTelefono,
     }=datos;  
 
-        useEffect(() =>{
+    useEffect(() =>{
         setStatus('loading')
         axios.get(`http://localhost:5000/prospects/${props.match.params.idProspect}`)
         .then((result) => {
@@ -35,7 +85,106 @@ function SolicitudCliente(props) {
         })
     },[])
 
-    //cambiar por el de diana
+    function handleChange(event){
+        let newApplication={
+            ...fullApplication,
+            [event.target.name]: event.target.value,
+        };
+        setFullApplication(newApplication)
+    }
+
+    const handleSelectChange = selectedOption => {
+        setSelectValue(selectedOption);
+    }
+
+    function handleSave(event){
+        event.preventDefault()
+        console.log(fullApplication)
+
+        const idClient= parseInt(props.match.params.idProspect)
+        const creditoSolicitado = SelectValue.value
+        const estatus = "No revisado"
+        const firmaSolicitud=1
+
+
+        const {
+            fechaNacimiento,
+            direccion,
+            numClienteZorro,
+            numIne,
+            montoSolicitado,
+            refName1,
+            numTelefonoReferencia1,
+            refName2,
+            numTelefonoReferencia2,
+            refName3,
+            numTelefonoReferencia3
+        }=fullApplication
+
+        const applicationBack={
+            client : {
+                idClient,
+                fechaNacimiento ,
+                numClienteZorro ,
+                numIne,
+                direccion,
+            },
+            
+            application : {
+                idClient ,
+                idAssessor ,
+                creditoSolicitado ,
+                montoSolicitado,
+                estatus,
+                firmaSolicitud 
+            },
+            
+            reference1 : {
+                idClient,
+                refName : refName1,
+                numTelefonoReferencia: numTelefonoReferencia1
+            },
+            
+            reference2 : {
+                idClient ,
+                refName : refName2,
+                numTelefonoReferencia: numTelefonoReferencia2
+            },
+            
+            reference3 : {
+                idClient ,
+                refName : refName3,
+                numTelefonoReferencia: numTelefonoReferencia3
+            }
+        }
+
+        console.log(applicationBack)
+        axios({
+            method: 'post',
+            url: 'http://localhost:5000/applications/clients/references',
+            data:{
+                body: applicationBack,
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                }
+            }
+        })
+        .then((result)=>{
+            console.log(result)
+            setStatus('resolved')
+        })
+        .catch((error)=>{
+            setStatus('error')
+        })
+        
+
+    }
+    
+    let fecha = new Date();
+    let varFecha = `${fecha.getDate()} de ${fecha.toLocaleString('default', { month: 'long' })} del ${fecha.getFullYear()}`;
+    let tabs = ["Administrar prospectos", "Agregar prospectos", "Administrar Clientes"];
+
+        //cambiar por el de diana
     if(status=== 'idle' || status === 'loading'){
        return<h1>loading...</h1>
     }
@@ -45,37 +194,6 @@ function SolicitudCliente(props) {
         return<h1>oops</h1>
     }
 
-    /*
-    function handleChange(event){
-        let newFecha={
-            ...datos,
-            [event.target.fechaNacimiento]: event.target.value,
-        };
-        setInfo(newFecha)
-    }
-    */
-    function handleSave(event){
-        event.preventDefault()
-        axios.post(`http://localhost:5000/applications/clients/references/`,{
-            body: JSON.stringify(SolicitudCliente),
-            headers:{
-                'Content-Type': 'application/json: charet=UTF-8'
-            }
-        })
-        .then(()=>{
-
-        })
-        .catch(err=>{
-
-        })
-    }
-
-    let fecha = new Date();
-    let varFecha = `${fecha.getDate()} de ${fecha.toLocaleString('default', { month: 'long' })} del ${fecha.getFullYear()}`;
-    let dropMenuType = {contenido: "Tipo de crédito*",}
-    let tabs = ["Administrar prospectos", "Agregar prospectos", "Administrar Clientes"];
-
-  
     if(status === 'resolved'){
         return(
             <main id='mainSolicitudCliente'>
@@ -94,53 +212,53 @@ function SolicitudCliente(props) {
                         </div>
                         <div className='checkSolicitud'>
                             <p>Solicitud firmada:</p>
-                            <Checkbox />
+                            <Checkbox setCheckTrue={setIsCheck}/>
                         </div>
                     </section>
-                    <form>
+                    <form  onSubmit={handleSave}>
                         <section className='inputsDatosCliente'>
                             <div className='grupoInput'>
-                                <input type='text' className='inputFormularios w-2'  placeholder='Fecha de nacimiento*' onFocus={(e) => {e.currentTarget.type = 'date'}} ></input>                            
+                                <input type='text' className='inputFormularios w-2' name="fechaNacimiento" placeholder='Fecha de nacimiento*' onFocus={(e) => {e.currentTarget.type = 'date'}} required onChange = {handleChange} ></input>                            
                                 <label htmlFor="name" className="etiquetaInputs">Fecha de nacimiento* (formato dd/mm/aaaa)</label>
                             </div>
                             <div className='grupoInput'>
-                                <input type='text' className='inputFormularios w-2' name="Dirección*" placeholder='Dirección*' required></input>
+                                <input type='text' className='inputFormularios w-2' name="direccion" placeholder='Dirección*' required  onChange = {handleChange}></input>
                                 <label htmlFor="name" className="etiquetaInputs">Dirección*(calle, número, ciudad, estado, código postal)</label>
                             </div>
                             <div className='grupoInput'>
-                                <input type='number' className='inputFormularios w-2' name='No. de cliente Zorro Abarrotero*' placeholder='No. de cliente Zorro Abarrotero*' required></input>
+                                <input type='number' className='inputFormularios w-2' name='numClienteZorro' placeholder='No. de cliente Zorro Abarrotero*' required  onChange = {handleChange}></input>
                                 <label htmlFor="name" className="etiquetaInputs">No. de cliente Zorro Abarrotero*</label>
                             </div>
                             <div className='grupoInput'>
-                                <input type='number' className='inputFormularios w-2' name='INE*' placeholder='INE*' required></input>
+                                <input type='number' className='inputFormularios w-2' name='numIne' placeholder='INE*' required  onChange = {handleChange}></input>
                                 <label htmlFor="name" className="etiquetaInputs">INE*(10 dígitos de la parte posterior)</label>
                             </div>
                             <div className='grupoInput'>
-                                <input type='number' className='inputFormularios w-2' name='Monto solicitado*' placeholder='Monto solicitado*' required></input>
+                                <input type='number' className='inputFormularios w-2' name='montoSolicitado' placeholder='Monto solicitado*' required  onChange = {handleChange}></input>
                                 <label htmlFor="name" className="etiquetaInputs">Monto solicitado*(coloque sólo el valor numérico)</label>
                             </div>
-                            <DropMenu {...dropMenuType}/>
+                            <Select placeholder = "Departamento"  options={tiposCredito} styles = {customSelectStyles} onChange = {handleSelectChange}/>
                         </section>
                         <section className='inputsReferenciasCliente'>
                             <div className='tarjetaReferencia'>
                                 <h1>Referencia 1:</h1>
-                                <input type='text' className='input-gral' placeholder='Nombre'></input>
-                                <input type='text' className='input-gral' placeholder='Teléfono'></input>
+                                <input type='text' className='input-gral' placeholder='Nombre' name="refName1"  onChange = {handleChange}></input>
+                                <input type='text' className='input-gral' placeholder='Teléfono' name='numTelefonoReferencia1'  onChange = {handleChange}></input>
                             </div>
                             <div className='tarjetaReferencia'>
                                 <h1>Referencia 2:</h1>
-                                <input type='text' className='input-gral' placeholder='Nombre'></input>
-                                <input type='text' className='input-gral' placeholder='Teléfono'></input>
+                                <input type='text' className='input-gral' placeholder='Nombre' name="refName2"  onChange = {handleChange}></input>
+                                <input type='text' className='input-gral' placeholder='Teléfono' name='numTelefonoReferencia2'  onChange = {handleChange}></input>
                             </div>
                             <div className='tarjetaReferencia'>
                                 <h1>Referencia 3:</h1>
-                                <input type='text' className='input-gral' placeholder='Nombre'></input>
-                                <input type='text' className='input-gral' placeholder='Teléfono'></input>
+                                <input type='text' className='input-gral' placeholder='Nombre' name="refName3"  onChange = {handleChange}></input>
+                                <input type='text' className='input-gral' placeholder='Teléfono' name='numTelefonoReferencia3'  onChange = {handleChange}></input>
                             </div>
                         </section>
                         <section className='botonesEnviarSolicitud'>
                             <CustomLink tag='button' to='/administrarProspectos' className="botonAzulMarino">Cancelar</CustomLink>
-                            <CustomLink tag='input' type='submit' className='botonSalmon' value='Enviar solicitud' to='/administrarProspectos' onClick={handleSave}/>
+                            <CustomLink tag='button' type='submit' className='botonSalmon' disabled={isCheck}>Enviar Solicitud</CustomLink>
                         </section>
                     </form>
                 </section>
